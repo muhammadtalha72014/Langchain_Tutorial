f2268de16f85a54822b405f8840657f5b1041ee1
Muhammad Talha <talhahamid1234@gmail.com>
2025-08-09 15:54:21 +0500

    Add files via upload

---
+++ 10.Docloader/1_textloader.py
@@ -0,0 +1,32 @@
+from langchain_community.document_loaders import TextLoader
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import PromptTemplate
+from dotenv import load_dotenv
+
+load_dotenv()
+
+from langchain_google_genai import ChatGoogleGenerativeAI
+model = ChatGoogleGenerativeAI(model="gemini-2.0-flash")
+
+prompt = PromptTemplate(
+    template='Write a summary for the following poem - \n {poem}',
+    input_variables=['poem']
+)
+
+parser = StrOutputParser()
+
+loader = TextLoader('cricket.txt', encoding='utf-8')
+
+docs = loader.load()
+
+print(type(docs))
+
+print(len(docs))
+
+print(docs[0].page_content)
+
+print(docs[0].metadata)
+
+chain = prompt | model | parser
+
+print(chain.invoke({'poem':docs[0].page_content}))
